3f61948851b0e4cd4e4bee1a0f110eb5e7a572f3
Angellyn218 <angellyncervantes@yahoo.com>
2022-10-21 09:14:21 -0700

    linked generateMarkdown.js to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [{ // Title
@@ -49,6 +50,7 @@ const questions = [{ // Title
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     console.log(data);
+    generateMarkdown(data);
     const dataStr = `# ${data.title}
      
 ## Description
